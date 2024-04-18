2b5c428e1b765221d93f3620859f35207d8e86eb
Nenad Brcic <neno@neno.ch>
2024-04-18 15:04:14 +0200

    Add eslint

--- src/math/add.ts
+++ src/math/add.ts
@@ -1,3 +1,17 @@
+const myArray = [
+  1,2,
+3
+]
+
+function doSomeStuff(
+arr) {
+  for (let nr of
+    arr) {
+      console.log("number: " + nr);
+      
+    }
+}
+
 export function add(a: number, b: number): number {
   return a + b;
 }
